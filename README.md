1e2aa91c5dcac4bb2980ac311319af1d84a8a7d1
Andrew Hood <andrewhood125@gmail.com>
2015-02-27 21:03:40 +0000

    Force psuedo tty

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # do-debian-setup
 Take a fresh install of Debian on DigitalOcean and spruce it up.
+
+Usage
+-----
+`curl
+https://raw.githubusercontent.com/andrewhood125/do-debian-setup/master/do-debian-setup.sh
+| bash -s DROPLET_IP_ADDR`
--- do-debian-setup.sh
+++ do-debian-setup.sh
@@ -1,3 +1,3 @@
-ssh root@$1 'adduser deployer && adduser deployer sudo'
-ssh deployer@$1 'ssh-keygen'
+ssh -t root@$1 'adduser deployer && adduser deployer sudo'
+ssh -t deployer@$1 'ssh-keygen'
 scp ~/.ssh/id_rsa.pub deployer@$1:"~/.ssh/authorized_keys"
