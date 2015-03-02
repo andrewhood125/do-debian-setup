02b347a7ee56b2eb70751d053d4ed3a4f6d8eea4
Andrew Hood <andrewhood125@gmail.com>
2015-03-02 17:01:18 +0000

    Install some important packages

--- do-debian-setup.sh
+++ do-debian-setup.sh
@@ -1,3 +1,7 @@
-ssh -t root@$1 'adduser deployer && adduser deployer sudo'
+ssh -t root@$1 'apt-get update && \
+                apt-get upgrade && \
+                apt-get install -y -q curl git htop vim && \
+                adduser deployer && \
+                adduser deployer sudo'
 ssh -t deployer@$1 'ssh-keygen'
 scp ~/.ssh/id_rsa.pub deployer@$1:"~/.ssh/authorized_keys"
