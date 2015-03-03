43a555fb4b30ab2e37dd14612b3f074d57154057
Andrew Hood <andrewhood125@gmail.com>
2015-03-03 17:41:36 +0000

    Optimize the script so not as many password prompts

--- do-debian-setup.sh
+++ do-debian-setup.sh
@@ -2,6 +2,9 @@ ssh -t root@$1 'apt-get update && \
                 apt-get upgrade -y && \
                 apt-get install -y -q curl git htop vim && \
                 adduser deployer --gecos "" && \
-                adduser deployer sudo'
-ssh -t deployer@$1 'ssh-keygen'
-scp ~/.ssh/id_rsa.pub deployer@$1:"~/.ssh/authorized_keys"
+                adduser deployer sudo && \
+                mkdir /home/deployer/.ssh && \
+                cp /root/.ssh/authorized_keys /home/deployer/.ssh/ && \
+                chown -R deployer:deployer /home/deployer/.ssh && \
+                chmod 600 /home/deployer/.ssh/authorized_keys && \
+                chmod 700 /home/deployer/.ssh'
