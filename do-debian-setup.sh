a53c2ac3c211dfdd22c034fc3cb4d7cec2227a5e
Andrew Hood <andrewhood125@gmail.com>
2015-03-05 12:56:17 -0600

    Default password with expired login

--- do-debian-setup.sh
+++ do-debian-setup.sh
@@ -1,8 +1,10 @@
 ssh -t root@$1 'apt-get update && \
                 apt-get upgrade -y && \
                 apt-get install -y -q curl git htop vim && \
-                adduser deployer --gecos "" && \
+                adduser deployer --disabled-password --gecos "" && \
                 adduser deployer sudo && \
+                echo "deployer:password" | chpasswd && \
+                passwd -e deployer && \
                 mkdir /home/deployer/.ssh && \
                 cp /root/.ssh/authorized_keys /home/deployer/.ssh/ && \
                 chown -R deployer:deployer /home/deployer/.ssh && \
