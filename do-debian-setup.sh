55ef65f04fc508f2af51d7b8f2482ad84506f09e
Andrew Hood <andrewhood125@gmail.com>
2015-03-05 22:58:15 -0600

    No fake tty

--- do-debian-setup.sh
+++ do-debian-setup.sh
@@ -1,12 +1,12 @@
-ssh -t root@$1 'apt-get update && \
-                apt-get upgrade -y && \
-                apt-get install -y -q curl git htop vim && \
-                adduser deployer --disabled-password --gecos "" && \
-                adduser deployer sudo && \
-                echo "deployer:password" | chpasswd && \
-                passwd -e deployer && \
-                mkdir /home/deployer/.ssh && \
-                cp /root/.ssh/authorized_keys /home/deployer/.ssh/ && \
-                chown -R deployer:deployer /home/deployer/.ssh && \
-                chmod 600 /home/deployer/.ssh/authorized_keys && \
-                chmod 700 /home/deployer/.ssh'
+ssh root@$1 'apt-get update && \
+            apt-get upgrade -y && \
+            apt-get install -y -q curl git htop vim && \
+            adduser deployer --disabled-password --gecos "" && \
+            adduser deployer sudo && \
+            echo "deployer:password" | chpasswd && \
+            passwd -e deployer && \
+            mkdir /home/deployer/.ssh && \
+            cp /root/.ssh/authorized_keys /home/deployer/.ssh/ && \
+            chown -R deployer:deployer /home/deployer/.ssh && \
+            chmod 600 /home/deployer/.ssh/authorized_keys && \
+            chmod 700 /home/deployer/.ssh'
